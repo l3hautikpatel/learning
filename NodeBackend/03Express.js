60d836a21c4bc43f4b400e798e04dce5e219491e
l3hautikpatel <bhautikp105@gmail.com>
2024-08-08 13:35:54 -0400

    added gitingore file

--- NodeBackend/03Express.js
+++ NodeBackend/03Express.js
@@ -1,6 +1,14 @@
 var express = require("express")
 var app = express();
 
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.use((req, res ,next)=>{
+    console.log("we are passed form the frist middelware");
+    next();
+})
+
 app.get('/',(req, res)=>{
     res.send("hello bhautik this is defult")
 })
@@ -12,5 +20,12 @@ app.get('/port',(req, res)=>{
     res.send("hello bhautik this is in port")
     
 })
+app.get('/error',(req, res,next)=>{
+    return next(new Error("some thing went worng and now i want to show that in the console that i can do in this "))
+})
 
+app.use((err ,req ,res ,next)=>{
+    console.error(err.stack);
+    res.status(500).send('something went wrong this is show in the responce part of the url')
+})
 app.listen(3000);
